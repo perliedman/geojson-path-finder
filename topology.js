f0ad813d9651d191154ec17d316f3fec40e33609
Per Liedman <per.liedman@dotnetmentor.se>
2016-12-30 21:47:43 +0100

    Only use vertices from LineStrings; pass on edge properties

--- topology.js
+++ topology.js
@@ -12,6 +12,22 @@ function geoJsonReduce(geojson, fn, seed) {
     }
 }
 
+function geoJsonFilterFeatures(geojson, fn) {
+    var features = [];
+    if (geojson.type === 'FeatureCollection') {
+        features = features.concat(geojson.features.filter(fn));
+    }
+
+    return {
+        type: 'FeatureCollection',
+        features: features
+    };
+}
+
+function isLineString(f) {
+    return f.geometry.type === 'LineString';
+}
+
 function topology(geojson, options) {
     options = options || {};
     var keyFn = options.keyFn || function(c) {
@@ -24,21 +40,20 @@ function topology(geojson, options) {
             });
         };
 
-    var vertices = explode(geojson).features.reduce(function(cs, f) {
+    var lineStrings = geoJsonFilterFeatures(geojson, isLineString);
+    var vertices = explode(lineStrings).features.reduce(function(cs, f) {
             var rc = roundCoord(f.geometry.coordinates);
             cs[keyFn(rc)] = f.geometry.coordinates;
             return cs;
         }, {}),
-        edges = geoJsonReduce(geojson, function(es, f) {
-            if (f.geometry.type === 'LineString') {
-                f.geometry.coordinates.forEach(function(c, i, cs) {
-                    if (i > 0) {
-                        var k1 = keyFn(roundCoord(cs[i - 1])),
-                            k2 = keyFn(roundCoord(c));
-                        es.push([k1, k2]);
-                    }
-                });
-            }
+        edges = geoJsonReduce(lineStrings, function(es, f) {
+            f.geometry.coordinates.forEach(function(c, i, cs) {
+                if (i > 0) {
+                    var k1 = keyFn(roundCoord(cs[i - 1])),
+                        k2 = keyFn(roundCoord(c));
+                    es.push([k1, k2, f.properties]);
+                }
+            });
 
             return es;
         }, []);
