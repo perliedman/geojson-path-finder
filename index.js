37955300e24ff595c855e21b668986ff781258cc
Per Liedman <per.liedman@dotnetmentor.se>
2017-01-06 23:49:11 +0100

    Refactor internal member names

--- index.js
+++ index.js
@@ -12,18 +12,13 @@ function PathFinder(graph, options) {
         graph = preprocess(graph, options);
     }
 
+    this._graph = graph;
     this._keyFn = options.keyFn || function(c) {
         return c.join(',');
     };
     this._precision = options.precision || 1e-5;
-    this._vertices = graph.vertices;
-    this._sourceVertices = graph.sourceVertices;
-    this._compact = {
-        graph: graph.compactedVertices,
-        coordinates: graph.compactedCoordinates
-    };
 
-    if (Object.keys(this._compact.graph).length === 0) {
+    if (Object.keys(this._graph.compactedVertices).length === 0) {
         throw new Error('Compacted graph contains no forks (topology has no intersections).');
     }
 }
@@ -36,7 +31,7 @@ PathFinder.prototype = {
         var phantomStart = this._createPhantom(start);
         var phantomEnd = this._createPhantom(finish);
 
-        var path = findPath(this._compact.graph, start, finish);
+        var path = findPath(this._graph.compactedVertices, start, finish);
 
         if (path) {
             var weight = path[0];
@@ -44,11 +39,11 @@ PathFinder.prototype = {
             return {
                 path: path.reduce(function buildPath(cs, v, i, vs) {
                     if (i > 0) {
-                        cs = cs.concat(this._compact.coordinates[vs[i - 1]][v]);
+                        cs = cs.concat(this._graph.compactedCoordinates[vs[i - 1]][v]);
                     }
 
                     return cs;
-                }.bind(this), []).concat([this._sourceVertices[finish]]),
+                }.bind(this), []).concat([this._graph.sourceVertices[finish]]),
                 weight: weight
             };
         } else {
@@ -60,24 +55,19 @@ PathFinder.prototype = {
     },
 
     serialize: function() {
-        return {
-            vertices: this._vertices,
-            sourceVertices: this._sourceVertices,
-            compactedVertices: this._compact.graph,
-            compactedCoordinates: this._compact.coordinates
-        };
+        return this._graph;
     },
 
     _createPhantom: function(n) {
-        if (this._compact.graph[n]) return null;
+        if (this._graph.compactedVertices[n]) return null;
 
-        var phantom = compactor.compactNode(n, this._vertices, this._compact.graph, this._sourceVertices, true);
-        this._compact.graph[n] = phantom.edges;
-        this._compact.coordinates[n] = phantom.coordinates;
+        var phantom = compactor.compactNode(n, this._graph.vertices, this._graph.compactedVertices, this._graph.sourceVertices, true);
+        this._graph.compactedVertices[n] = phantom.edges;
+        this._graph.compactedCoordinates[n] = phantom.coordinates;
 
         Object.keys(phantom.incomingEdges).forEach(function(neighbor) {
-            this._compact.graph[neighbor][n] = phantom.incomingEdges[neighbor];
-            this._compact.coordinates[neighbor][n] = phantom.incomingCoordinates[neighbor];
+            this._graph.compactedVertices[neighbor][n] = phantom.incomingEdges[neighbor];
+            this._graph.compactedCoordinates[neighbor][n] = phantom.incomingCoordinates[neighbor];
         }.bind(this));
 
         return n;
@@ -86,14 +76,14 @@ PathFinder.prototype = {
     _removePhantom: function(n) {
         if (!n) return;
 
-        Object.keys(this._compact.graph[n]).forEach(function(neighbor) {
-            delete this._compact.graph[neighbor][n];
+        Object.keys(this._graph.compactedVertices[n]).forEach(function(neighbor) {
+            delete this._graph.compactedVertices[neighbor][n];
         }.bind(this));
-        Object.keys(this._compact.coordinates[n]).forEach(function(neighbor) {
-            delete this._compact.coordinates[neighbor][n];
+        Object.keys(this._graph.compactedCoordinates[n]).forEach(function(neighbor) {
+            delete this._graph.compactedCoordinates[neighbor][n];
         }.bind(this));
 
-        delete this._compact.graph[n];
-        delete this._compact.coordinates[n];
+        delete this._graph.compactedVertices[n];
+        delete this._graph.compactedCoordinates[n];
     }
 };
