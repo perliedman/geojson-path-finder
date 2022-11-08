7ca55a108d9ea00e58d27b9ba6ec48c32089e576
Per Liedman <per@liedman.net>
2022-11-08 21:39:10 +0100

    Add some docs

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,39 @@
 import { Position } from "@turf/helpers";
 
+/**
+ * Vertex key, a unique identifier for a vertex of a graph
+ */
 export type Key = string;
+
+/**
+ * Edge from A to B, containing its vertex keys and associated properties
+ */
 export type Edge<TProperties> = [Key, Key, TProperties];
+
+/**
+ * A topology of coordinates and their connecting edges
+ */
 export type Topology<TProperties> = {
   vertices: Coordinates;
   edges: Edge<TProperties>[];
 };
 
+/**
+ * A graph vertex, containing the edges
+ * connecting it to other vertices;
+ * edges are described as a lookup withthe target vertex's
+ * key associated to the edge's weight
+ */
 export type Vertex = Record<Key, number>;
+
+/**
+ * A set of vertices, indexed by their keys.
+ */
 export type Vertices = Record<Key, Vertex>;
+
+/**
+ *
+ */
 export type Coordinates = Record<Key, Position>;
 
 export type PathFinderGraph<TEdgeData> = {
